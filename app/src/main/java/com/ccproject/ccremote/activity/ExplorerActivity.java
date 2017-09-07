372fbd6d3579f4fc094778302d4db39ced753649
CC <261909098@qq.com>
2017-09-07 22:11:43 +0800

    RecyclerView增加多选

--- app/src/main/java/com/ccproject/ccremote/activity/ExplorerActivity.java
+++ app/src/main/java/com/ccproject/ccremote/activity/ExplorerActivity.java
@@ -1,8 +1,12 @@
 package com.ccproject.ccremote.activity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -11,7 +15,9 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ccproject.ccremote.R;
 import com.ccproject.ccremote.Tools;
@@ -19,6 +25,14 @@ import com.ccproject.ccremote.adapter.FileAdapter;
 import com.ccproject.ccremote.connection.LocalServer;
 import com.ccproject.ccremote.item.Disk;
 import com.ccproject.ccremote.item.FileSystemEntry;
+import com.goyourfly.multiple.adapter.MultipleAdapter;
+import com.goyourfly.multiple.adapter.MultipleSelect;
+import com.goyourfly.multiple.adapter.menu.CustomMenuBar;
+import com.goyourfly.multiple.adapter.menu.MenuController;
+import com.goyourfly.multiple.adapter.menu.SimpleDeleteSelectAllMenuBar;
+import com.goyourfly.multiple.adapter.viewholder.view.CheckBoxFactory;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
@@ -28,7 +42,8 @@ import java.util.Vector;
 public class ExplorerActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener
 {
 	private List<FileSystemEntry> mFileList;
-	private FileAdapter mAdapter;
+	//private FileAdapter mAdapter;
+	private MultipleAdapter mAdapter;
 
 	private DrawerLayout mDrawerLayout;
 	private SwipeRefreshLayout mSwipeRefresh;
@@ -69,10 +84,8 @@ public class ExplorerActivity extends BaseActivity implements SwipeRefreshLayout
 		mRecyclerView = (RecyclerView) findViewById(R.id.Explorer_RecyclerView);
 		LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 		mRecyclerView.setLayoutManager(layoutManager);
-		mAdapter = new FileAdapter(mFileList);
-		mRecyclerView.setAdapter(mAdapter);
-		mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-		mAdapter.setOnItemClickListener((file)->
+		FileAdapter adapter = new FileAdapter(mFileList);
+		adapter.setOnItemClickListener((file)->
 		{
 			if (file.isDirectory())
 			{
@@ -83,6 +96,17 @@ public class ExplorerActivity extends BaseActivity implements SwipeRefreshLayout
 			else
 				;// TODO
 		});
+		mAdapter = MultipleSelect
+				.with(this)
+				.adapter(adapter)
+				.decorateFactory(new CheckBoxFactory())
+				.customMenu(new MultiSelectMenuBar(this,
+						R.menu.explorer_multiselect,
+						ContextCompat.getColor(ExplorerActivity.this, R.color.colorPrimary),
+						Gravity.TOP))
+				.build();
+		mRecyclerView.setAdapter(mAdapter);
+		mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 	}
 
 	private void initSwipeRefresh()
@@ -113,14 +137,14 @@ public class ExplorerActivity extends BaseActivity implements SwipeRefreshLayout
 		{
 			if (currentPath != null && !currentPath.equals("")) // 获取目录信息
 				myApplication.mLocalServer.sendForResponse(LocalServer.GET_FILE_SYSTEM_ENTRIES,
-											currentPath.getBytes(), bytes->refreshList(bytes, false));
+											currentPath.getBytes(), bytes->refreshList(bytes, false, true));
 			else // 获取磁盘信息
 				myApplication.mLocalServer.sendForResponse(LocalServer.GET_DISKS,
-											new byte[]{},  bytes->refreshList(bytes, true));
+											new byte[]{},  bytes->refreshList(bytes, true, true));
 		}).start();
 	}
 
-	private void refreshList(byte[] bytes, boolean isDisk)
+	private void refreshList(byte[] bytes, boolean isDisk, boolean scrollToFirst)
 	{
 		mFileList.clear();
 		int cursor = 0;
@@ -167,10 +191,38 @@ public class ExplorerActivity extends BaseActivity implements SwipeRefreshLayout
 		}
 		runOnUiThread(()->
 		{
+			mAdapter.cancel(false);
 			mAdapter.notifyDataSetChanged();
 			mSwipeRefresh.setRefreshing(false);
-			mRecyclerView.scrollToPosition(0);
+			if (scrollToFirst)
+				mRecyclerView.scrollToPosition(0);
 		});
+	}
+
+	private class MultiSelectMenuBar extends CustomMenuBar
+	{
+		public MultiSelectMenuBar(@NotNull Activity activity, int menuId, int menuBgColor, int gravity)
+		{
+			super(activity, menuId, menuBgColor, gravity);
+		}
+
+		@Override
+		public void onMenuItemClick(@NotNull MenuItem menuItem, @NotNull MenuController menuController)
+		{
+			List<Integer> select = mAdapter.getSelect();
+			switch (menuItem.getItemId()) // TODO
+			{
+				case R.id.explorer_multiselect_copy:
+					Toast.makeText(ExplorerActivity.this, select.size()+" items will be copied.", Toast.LENGTH_SHORT).show();
+					break;
+				case R.id.explorer_multiselect_cut:
+					Toast.makeText(ExplorerActivity.this, select.size()+" items will be cut.", Toast.LENGTH_SHORT).show();
+					break;
+				case R.id.explorer_multiselect_delete:
+					Toast.makeText(ExplorerActivity.this, select.size()+" items will be deleted.", Toast.LENGTH_SHORT).show();
+					break;
+			}
+		}
 	}
 
 	public static void actionStart(Context context)
